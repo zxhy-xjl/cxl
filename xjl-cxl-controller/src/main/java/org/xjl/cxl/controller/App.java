8bc6fe0e114085f24931714d269c3994dcda6eae
sunwii <806415118@qq.com>
2016-06-29 10:45:28 +0800

    初始化

---
+++ xjl-cxl-controller/src/main/java/org/xjl/cxl/controller/App.java
@@ -0,0 +1,13 @@
+package org.xjl.cxl.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
